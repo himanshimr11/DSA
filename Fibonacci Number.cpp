59a34e722e4879c22c2bc4fabf846e4e86b96c7e
HIMANSHI SHARMA <himanshisharma1110@gmail.com>
2025-02-14 12:07:37 +0530

    Leetcode 509

---
+++ Fibonacci Number.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/fibonacci-number/
+class Solution {
+public:
+    int fib(int n) {
+        int a=0,b=1,sum=0;
+        if(n==1)
+        {
+            return 1;
+        }
+        for(int i=2;i<=n;i++)
+        {
+            sum=a+b;
+            a=b;
+            b=sum;
+        }
+        return sum;
+           
+        
+    }
+};
