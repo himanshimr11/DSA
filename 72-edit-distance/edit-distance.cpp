f507254915041d24b3ce66a8b9dda70ff24acd08
HIMANSHI SHARMA <himanshisharma1110@gmail.com>
2025-10-27 10:38:51 +0530

    Time: 9 ms (38.94%) | Memory: 13 MB (64.85%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -15,8 +15,7 @@ public:
             dp[i][j]=1+min({
                 dp[i-1][j],
                 dp[i][j-1],
-                dp[i-1][j-1],
-        });
+                dp[i-1][j-1] });
         }
       }
       return dp[n][m];
