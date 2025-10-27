149569ea312c63a1be72960032f9fe270e0b75be
HIMANSHI SHARMA <himanshisharma1110@gmail.com>
2025-10-27 10:38:30 +0530

    Time: 7 ms (61.01%) | Memory: 13.3 MB (37.18%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,11 +4,9 @@ public:
       int   n= word1.size(), m= word2.size();
       vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
       for(int i=0;i<=n;i++){
-        dp[i][0]= i;
-      }
+        dp[i][0]= i;}
       for(int j=0;j<=m;j++){
-        dp[0][j] = j;
-      }
+        dp[0][j] = j;}
       for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
             if(word1[i-1] == word2[j-1])
