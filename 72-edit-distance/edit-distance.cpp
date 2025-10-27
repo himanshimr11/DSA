a1338ada78eb57f537c9cd97ddce7ae622c8e470
HIMANSHI SHARMA <himanshisharma1110@gmail.com>
2025-10-27 10:39:37 +0530

    Time: 9 ms (38.94%) | Memory: 13.1 MB (64.85%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,9 +4,11 @@ public:
       int   n= word1.size(), m= word2.size();
       vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
       for(int i=0;i<=n;i++){
-        dp[i][0]= i;}
+        dp[i][0]= i;
+        }
       for(int j=0;j<=m;j++){
-        dp[0][j] = j;}
+        dp[0][j] = j;
+        }
       for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
             if(word1[i-1] == word2[j-1])
@@ -15,10 +17,10 @@ public:
             dp[i][j]=1+min({
                 dp[i-1][j],
                 dp[i][j-1],
-                dp[i-1][j-1] });
+                dp[i-1][j-1]
+                 });
         }
       }
       return dp[n][m];
-
-    }
+      }
 };
